8dcda17c4a414c336fc543d9d408fb84f1894c79
FoxMD <ryusei@seznam.cz>
2022-04-25 21:10:54 +0200

    Cpp memory

--- memorymem.cpp
+++ memorymem.cpp
@@ -13,7 +13,10 @@ int main()
     free(p);
     p = NULL;   // needs to be set to NULL /dying pointer
 
-
-
-
+    // For Cpp new/delete
+    int *q = new int;   // or new int(5); for init with number
+    *q = 6;             // initialize with number
+    delete q;
+    q = nullptr;
+    // new can call Constructor
 }
