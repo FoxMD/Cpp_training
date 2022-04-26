fe06bb366739af374fd6312304b3dbaefa2b1127
FoxMD <ryusei@seznam.cz>
2022-04-26 19:47:41 +0200

    CopyTypes

---
+++ object_basic.cpp
@@ -0,0 +1,57 @@
+#include "iostream"
+
+class Integer{
+    int* m_pInt;
+    public:
+        Integer();
+        Integer(int value);
+        ~Integer();
+        int GetValue()const;
+        void SetValue(int value);
+};
+
+int Add(int x, int y)
+{
+
+}
+
+int main()
+{
+    int *p1 = new int(5);
+    int *p2 = p1; 
+    // thats a shallow copy 
+
+    int *p3 = new int(*p1);
+    // deep copy
+
+    // Always try to avoid copy, but if you must use deep copy (implement copy operator)
+    Integer myInt(5);
+    Integer myInt2(myInt); // copy of constructor ... can be copied also with return Integer etc
+    std::cout << myInt.GetValue() << std::endl;
+}
+
+Integer::Integer()
+{
+    m_pInt = new int(0);
+}
+
+
+Integer::Integer(int value)
+{
+    m_pInt = new int(value);
+}
+
+int Integer::GetValue()const
+{
+    return *m_pInt;
+}
+
+void Integer::SetValue(int value)
+{
+    *m_pInt = value;
+}
+
+Integer::~Integer()
+{
+    delete m_pInt;
+}
