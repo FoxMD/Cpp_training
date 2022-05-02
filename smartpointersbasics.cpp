7fcdd27990a6be1b24ed8dc927979bafad5c6156
FoxMD <ryusei@seznam.cz>
2022-05-02 21:26:49 +0200

    shared, move, unique pointers

--- smartpointersbasics.cpp
+++ smartpointersbasics.cpp
@@ -1,5 +1,6 @@
 #include "iostream"
 #include "Integer.h"
+#include <memory>   // smart pointers
 
 class IntPtr 
 {
@@ -10,6 +11,16 @@ class IntPtr
         Integer *operator->() {return m_p;}
         Integer & operator *() {return *m_p;}
 };
+
+void Process(std::unique_ptr<Integer> ptr)
+{
+    std::cout << ptr->GetValue() << std::endl;
+}
+
+void ProcessShared(std::shared_ptr<Integer> ptr)
+{
+    std::cout << ptr->GetValue() << std::endl;
+}
 
 void CreateInteger() 
 {
@@ -22,6 +33,16 @@ void CreateInteger()
     IntPtr p = new Integer; // behavior like a pointer but it is a local object - type of Smart pointer
     p->SetValue(3);     // with overload -> 
     (*p).SetValue(3);   // with overload *
+
+    std::unique_ptr<Integer> q(new Integer); // the same as  IntPtr q = new Integer;, will be deleted after... we cant make a copy of unique pointer
+    // auto q2(q); is not possible it is a deleted pointer, so it has only move semantics
+    // Process(p); is also not possible due to copy
+    Process(std::move(q));  // transfers ownership to ptr after the scope it will be released
+
+    std::shared_ptr<Integer> t(new Integer);    // if we want to share the resource in code, its incremented per share and decremented per scope release
+    t->SetValue(5);
+    ProcessShared(t);
+    std::cout << t->GetValue() << std::endl;
 }
 
 int main()
