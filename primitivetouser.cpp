61248b873ac3c9f4ff50838828145e5dee760ab3
FoxMD <ryusei@seznam.cz>
2022-05-04 20:48:35 +0200

    Initialization vs. assignment v1

--- primitivetouser.cpp
+++ primitivetouser.cpp
@@ -13,5 +13,11 @@ int main()
     // user to primitive
     // type conversion operator ... operator<type>() no arg or return
     int x = static_cast<int>(a); // calls Integer::operator int() {return *m_pInt;} it is recomendet to use explicit (without call is int x = a;), with static_cast
+
+    // Initialization vs Assignment
+    Integer c(5);   // calls Integer() then 5 and ~Integer()
+    Integer d;      // calls Integer(), Integer(int), operator=(Integer&&),~Integer(), 6, ~Integer() 
+    d = 5;          // always prefer initialization over assignment
+
     return 0;
 }
