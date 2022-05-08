698e0cee90fc1ed658a9b035125962a2a68bb942
FoxMD <ryusei@seznam.cz>
2022-05-08 10:29:12 +0200

    weak

--- weakownership.cpp
+++ weakownership.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
-
+// muliple weak pointers can count to one shared pointer
+// expired checks reference count
+// helps to access resource that may or may not be availiable, if resource is availiable we lock to it and create a sp
 class Printer 
 {
-    std::shared_ptr<int> m_pValue{};
+    std::weak_ptr<int> m_pValue{};
     public:
-        void SetValue(std::shared_ptr<int> p) {m_pValue = p;}
-        void Print() { std::cout << *m_pValue << std::endl;}
+        void SetValue(std::weak_ptr<int> p) {m_pValue = p;}
+        void Print() 
+        {
+            if(m_pValue.expired()) {std::cout << "Already expired" << std::endl; return;}
+            auto sp = m_pValue.lock(); 
+            std::cout << *sp << std::endl;
+        }
 };
 
 int main()
