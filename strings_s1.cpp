95e7ae4d46319be8ac13d940bfd4a4408576768a
FoxMD <ryusei@seznam.cz>
2022-05-08 19:35:06 +0200

    modded

--- strings_s1.cpp
+++ strings_s1.cpp
@@ -3,7 +3,8 @@
 
 const char* Combine(const char* pFirst, const char* pLast)
 {
-    char fullname[20];
+    //char fullname[20];
+    char *fullname = new char[strlen(pFirst) + strlen(pLast) + 1]; // + null termination
     strcpy(fullname, pFirst);
     strcat(fullname, pLast);
     return fullname;
@@ -16,6 +17,9 @@ int main()
     std::cin.getline(first, 10);
     std::cin.getline(last, 10);
     const char* pFullName = Combine(first, last);   // wont work, local scope of fullname in Combine
-
+ 
     std::cout << pFullName << std::endl;
+    delete[] pFullName;
+
+    return 0;
 }
