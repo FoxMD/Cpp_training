543e98fcf0b6d70679c0f08626372d477f243c1f
FoxMD <ryusei@seznam.cz>
2022-05-14 20:52:42 +0200

    Union 2

--- union1.cpp
+++ union1.cpp
@@ -4,14 +4,28 @@ union Test
 {
     int x;
     char c;
-    Test(): x{1}{}
+    Test(): x{1}{ std::cout << __FUNCSIG__ <<std::endl;}
     ~Test() {}
 };
 
+// A and B wont be created explicitly needs to be done manually
+/*
+union UDT
+{
+    A a;
+    B b;
+    UDR(){}
+    ~UDT(){}
+};
+*/
 int main()
 {
     Test t;
     t.x = 100;
-
+/*
+    UDT udt;
+    new(&udt.a) A{}; // initialize it, memory is already reserved
+    udt.a.~A();
+*/
     return 0;
 }
