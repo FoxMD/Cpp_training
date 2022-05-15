215ad4a879c1db7a67be94985ae24af298bc089f
FoxMD <ryusei@seznam.cz>
2022-05-15 20:07:56 +0200

    Inheritance

---
+++ inheritance.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+class Animal
+{
+    public:
+        void Eat() {}
+        void Run() {}
+        void Speak() {}
+};
+
+class Dog : public Animal
+{
+
+};
+
+int main()
+{
+
+    return 0;
+}
