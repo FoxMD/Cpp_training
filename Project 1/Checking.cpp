e646916d7d74cf12351c820bb24f7004ecdcb36e
FoxMD <ryusei@seznam.cz>
2022-05-17 21:46:00 +0200

    Inheritance constructors

--- Project 1/Checking.cpp
+++ Project 1/Checking.cpp
@@ -1,10 +1,12 @@
 #include "Checking.h"
-
 #include <iostream>
+
+// In C++ 3 things are not inherited, Constructor, destructor and assignment operator
 Checking::Checking(const std::string &name, float balance, float minbalance):
 m_MinimumBalance(minbalance), Account(name, balance){
 }
-
+// in C++ we can use inheriting constructors so we dont need to implement constructors for child classes
+// to use it you need to implement using keyword: using Account::Account; 
 
 Checking::~Checking() {
 }
