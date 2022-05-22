ce091583566b680936378e0f611187825ec7fd52
FoxMD <ryusei@seznam.cz>
2022-05-22 19:28:20 +0200

    Numeric limits

--- Project 1/exceptionhandling.cpp
+++ Project 1/exceptionhandling.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 
+void ProcessRecors(int count)
+{
+    int *pArray = (int*)malloc(count * sizeof(int));
+    if(pArray == nullptr) return;
+    for(int i = 0; i < count; ++i)
+    {
+        pArray[i] = i; 
+    }
+    free(pArray);
+}
+
 int main()
 {
+    ProcessRecors(std::numeric_limits<int>::max());
     return 0; 
 }
