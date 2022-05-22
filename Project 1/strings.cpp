80553cdf534f0364e4d85e3bd5c7aeaeef85bb58
FoxMD <ryusei@seznam.cz>
2022-05-22 20:40:18 +0200

    strings

---
+++ Project 1/strings.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string rawstring{R"(C:\ignored\backslach)"};
+    std::string specialchar{R"MSG(This is my super C++ special message with everthing "(In C++17)")MSG"};
+    return 0;
+}
