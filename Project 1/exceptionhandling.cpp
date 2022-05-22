558c62463f5e587c868b08ce9af20ad6cb02ae31
FoxMD <ryusei@seznam.cz>
2022-05-22 19:32:19 +0200

    no except but better

--- Project 1/exceptionhandling.cpp
+++ Project 1/exceptionhandling.cpp
@@ -3,7 +3,10 @@
 void ProcessRecors(int count)
 {
     int *pArray = (int*)malloc(count * sizeof(int));
-    if(pArray == nullptr) return;
+    if(pArray == nullptr)    // return not good user doesnt know what happend
+    {
+        throw std::runtime_error("Failed to allocate memory");
+    }
     for(int i = 0; i < count; ++i)
     {
         pArray[i] = i; 
