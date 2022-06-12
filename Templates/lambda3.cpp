b235d5ec244e001f2a50c9173e20ccee609ae2cc
FoxMD <ryusei@seznam.cz>
2022-06-12 18:48:43 +0200

    lambda mutable

--- Templates/lambda3.cpp
+++ Templates/lambda3.cpp
@@ -32,6 +32,15 @@ struct Comp2
     bool operator()(int x, int y) { return x > y; }
 };
 
+template<typename T, int size, typename Callback>
+void ForEach(T(&arr)[size], Callback operation)
+{
+    for(int i = 0; i < size -1; ++i )
+    {
+        operation(arr[i]);
+    }
+}
+
 int main()
 {
     Comp2 comp;
@@ -47,5 +56,28 @@ int main()
     {
         std::cout << x << ", ";
     }
+
+    ForEach(arr, [](auto x) { std::cout << x << std::endl;});
+    
+    int offset = 4;
+    ForEach(arr, [offset](auto &x) { x += offset;});    // capture list [] contains some local variable, but it cannot be modified
+    
+    // to access and modify the value in capture list we need to use mutable
+    ForEach(arr, [offset](auto &x) mutable { x += offset; ++offset;});
+    
     return 0;
 }
+
+/*  Implementation from compiler
+template<typename T>
+struct Unnamed
+{
+    int offset;
+    Unnamed() : offset(off) const {}    // if we use mutable there is no const
+    void operator()(T &x)
+    {
+        x += offset;
+        ++offset;
+    }
+};
+*/
