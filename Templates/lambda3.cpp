f737330e2bc1bd75928a52d0c0f21c1fb480a4fe
FoxMD <ryusei@seznam.cz>
2022-06-12 18:36:56 +0200

    Lambda

---
+++ Templates/lambda3.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+template<typename T, int size, typename Comparator>
+void Sort(T(&arr)[size], Comparator comp)
+{
+    for(int i = 0; i < size -1; ++i)
+    {
+        for(int j = 0; j < size -1; ++j)
+        {
+            if(comp(arr[j], arr[j + 1]))
+            {
+                T temp = std::move(arr[j]);
+                arr[j] = std::move(arr[j + 1]);
+                arr[j + 1] = std::move(temp);
+            }
+        }
+    }
+}
+
+bool Comp(int x, int y)
+{
+    return x > y;
+}
+
+bool Comp1(int x, int y)
+{
+    return x < y;
+}
+
+struct Comp2 
+{
+    bool operator()(int x, int y) { return x > y; }
+};
+
+int main()
+{
+    Comp2 comp;
+    comp(3, 5); // comp.operator()(3, 5)
+
+    int arr[]{1,4,9,8,0,3};
+    for(auto x: arr)
+    {
+        std::cout << x << ", ";
+    }
+    Sort(arr, [](auto x, auto y) { return x > y;});
+    for(auto x: arr)
+    {
+        std::cout << x << ", ";
+    }
+    return 0;
+}
