ee8e17df679d551bd656e4a1dd8400a7318e8d55
FoxMD <ryusei@seznam.cz>
2022-06-16 20:42:07 +0200

    Unordnered list

--- Templates/unorderedcontainer.cpp
+++ Templates/unorderedcontainer.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <unordered_set>
+#include <unordered_map>
 #include <string>
 
 void UnSet()
@@ -20,6 +21,19 @@ void UnSet()
     std::cout << "Bucket count: " << coll.bucket_count() << std::endl;
     std::cout << "Number of elements: " << coll.size() << std::endl;
     std::cout << "Load factor: " << coll.load_factor() << std::endl; 
+}
+
+void UnMap()
+{
+    std::unordered_map<std::string, std::string> coll;
+    coll["Batman"] = "Bruce Wayne";
+    coll["Superman"] = "Clark Kent";
+    coll["Hulk"] = "Bruce Banner";
+
+    for(const auto &element: coll)
+    {
+        std::cout << element.first << ":" << element.second << std::endl;
+    }
 }
 
 int main()
