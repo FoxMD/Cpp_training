5cd4cb5cf6931580290503553486efaa52d25c87
FoxMD <ryusei@seznam.cz>
2022-06-17 16:07:35 +0200

    threads

--- Concurency/threads_part1.cpp
+++ Concurency/threads_part1.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 #include <list>
 #include <string>
+#include <thread>
 
+/*  std::thread
+    - accepts a callable and executes in a separate thread
+    - constructor does not wait for the thread to start, it returns immediately
+        - we need to wait for the child thread to finish
+*/
 std::list<int> g_Data;
 const size_t SIZE{5000000};
 
@@ -16,9 +22,20 @@ void Download()
 
 int main()
 {
-    std::cout << R"MSG([Main] User started download)MSG" << std::endl;
-    Download(); // takes time
-    std::cout << R"MSG([Main] User started ...)MSG" << std::endl;
+//    std::cout << R"MSG([Main] User started download)MSG" << std::endl;
+//    std::thread thDownaloader(Download);
+//    std::cout << R"MSG([Main] User started ...)MSG" << std::endl;
 
+//    thDownaloader.join();   // call before the object is destroyed
+    // if we dont wanna wait we can:
+    std::cout << R"MSG([Main] User started download)MSG" << std::endl;
+    std::thread thDownaloader(Download);
+    thDownaloader.detach();
+    std::cout << R"MSG([Main] User started ...)MSG" << std::endl;
+    // exists before finishing the thread
+    if(thDownaloader.joinable())    
+        thDownaloader.join();
+    
+    system("pause");    // we see detached task to finish
     return 0;
 }
